d058598253f63329de31950c4fa1b89abdd291fe
sclark39 <sclark@freolic.com>
2009-10-28 17:52:12 -0700

    wtf?

--- GNET/Shared/Base/GNET_GamePacket.h
+++ GNET/Shared/Base/GNET_GamePacket.h
@@ -10,24 +10,5 @@ namespace GNET {
 		virtual char GetType() = 0;
 	};
 
-	struct GamePktReg {	
-		int size;
-		IGamePacket* (*instantiate) (void);
-	};
-
-	#define REGISTER_PACKET(T)	{								\
-		GNET::GamePktReg pkt_data; T t;							\
-		pkt_data.size = sizeof(T);								\
-		pkt_data.instantiate = t.Instantiate;					\
-		GNET::g_GamePackets.insert(								\
-			std::pair<char,GNET::GamePktReg>(t.GetType(), pkt_data)); }
-
-	#define PACKET_TYPE(num,x)								\
-		char GetType() { return num; }						\
-		static IGamePacket* Instantiate() { return new x; }
-
-
-	extern std::map<char,GamePktReg> g_GamePackets;
-	
-	DataPack* CreateDataPack(IGamePacket* packet);
+	extern PktRegMap g_GamePackets;	
 }
